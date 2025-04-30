30c7ab6263f0902a7591abaf6211ee2966793c62
achsanalfitra <achsanalfitra@gmail.com>
2025-04-30 18:39:33 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/nz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:nz/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
