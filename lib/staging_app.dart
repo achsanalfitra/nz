e126fb0259c80662598e9b20472267fab7354096
achsanalfitra <achsanalfitra@gmail.com>
2025-04-30 18:58:56 +0700

    chore: add basic staging app w/ scaffold and center

---
+++ lib/staging_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StagingApp extends StatelessWidget {
+  const StagingApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Text("Placeholder"))),
+    ); //insert your app here
+  }
+}
