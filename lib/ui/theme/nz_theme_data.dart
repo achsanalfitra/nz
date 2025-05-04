311db2de6c44704fbbc4596a9dab32db6171b632
Alfitra Achsan <achsanalfitra@gmail.com>
2025-05-04 19:52:17 +0700

    feat(theme): add NzColourPaletter to NzThemeData

--- lib/ui/theme/nz_theme_data.dart
+++ lib/ui/theme/nz_theme_data.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 class NzThemeData {
-  final Color primaryColour;
-  final Color secondaryColour;
+  final NzColourPalette primaryColour;
+  final NzColourPalette secondaryColour;
 
   NzThemeData({
     required this.primaryColour,
     required this.secondaryColour,
+  });
+}
+
+class NzColourPalette {
+  final Color? colour100;
+  final Color? colour300;
+  final Color? colour500;
+  final Color? colour700;
+  final Color? colour900;
+
+  NzColourPalette({
+    required this.colour100,
+    required this.colour300,
+    required this.colour500,
+    required this.colour700,
+    required this.colour900,
   });
 }
