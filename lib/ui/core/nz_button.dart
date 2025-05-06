a9b1fd0337e365af119a27f43ee1738034e0b624
Alfitra Achsan <achsanalfitra@gmail.com>
2025-05-06 14:48:42 +0700

    feat(button): add active and inactive state then implement the inactive colours properly

--- lib/ui/core/nz_button.dart
+++ lib/ui/core/nz_button.dart
@@ -3,9 +3,14 @@ import 'package:google_fonts/google_fonts.dart';
 
 class NzButton extends StatefulWidget {
   final bool hasBorder;
+  final bool isActive;
+  final bool hasText;
   final Color background;
   final Color borderColour;
   final Color fontColor;
+  final Color inactiveBackground;
+  final Color inactiveBorderColour;
+  final Color inactiveFontColor;
   final double fontSize;
   final FontWeight fontWeight;
   final String buttonText;
@@ -18,9 +23,14 @@ class NzButton extends StatefulWidget {
 
   const NzButton({
     this.hasBorder = true,
+    this.hasText = true,
+    this.isActive = true,
     required this.background,
     required this.borderColour,
     required this.fontColor,
+    required this.inactiveBackground,
+    required this.inactiveBorderColour,
+    required this.inactiveFontColor,
     this.fontSize = 24,
     this.fontWeight = FontWeight.w500,
     this.buttonText = 'Button',
@@ -45,10 +55,10 @@ class _NzButtonState extends State<NzButton> {
       child: Container(
         padding: EdgeInsets.all(widget.padding),
         decoration: BoxDecoration(
-          color: widget.background,
+          color: widget.isActive? widget.background : widget.inactiveBackground,
           borderRadius: BorderRadius.circular(widget.borderRadius),
           border: Border.all(
-            color: widget.borderColour,
+            color: widget.isActive? widget.borderColour : widget.inactiveBorderColour,
             width: widget.hasBorder ? widget.borderWidth : 0,
           ),
         ),
@@ -58,15 +68,15 @@ class _NzButtonState extends State<NzButton> {
           spacing: 10,
           children: [
             if(widget.leftChildren != null) ...widget.leftChildren!,
-            Text( 
+            widget.hasText? Text( 
               widget.buttonText,
               style: GoogleFonts.montserrat(
                 fontSize: widget.fontSize,
                 fontWeight: widget.fontWeight,
                 letterSpacing: -0.03,
-                color: widget.fontColor,
+                color:  widget.isActive? widget.fontColor : widget.inactiveFontColor,
               ),
-            ),
+            ) : SizedBox.shrink(),
             if(widget.rightChildren != null) ...widget.rightChildren!,
           ],
         ),
