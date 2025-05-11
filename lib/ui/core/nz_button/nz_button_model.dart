d8f1d5d8bc23ae62d029d285b9dd0b24ccb97267
Alfitra Achsan <achsanalfitra@gmail.com>
2025-05-12 01:30:44 +0700

    feat(icon buttons): implement main icon button type with the helper svg function and fix rightchildren:leftchildren bugs

--- lib/ui/core/nz_button/nz_button_model.dart
+++ lib/ui/core/nz_button/nz_button_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nz/ui/core/nz_button/nz_button.dart';
 import 'package:nz/ui/theme/nz_theme.dart';
+import 'package:nz/helpers/svg_parser.dart';
 
 class NzButtonModel {
   // static Widget _iconParser(String iconName) {
@@ -62,7 +63,7 @@ class NzButtonModel {
         borderWidth: borderWidth,
         borderRadius: borderRadius ?? 10,
         leftChildren: leftChildren,
-        rightChildren: leftChildren,
+        rightChildren: rightChildren,
         onTap: onTap,
       );
     } else if (buttonName == 'tertiary') {
@@ -83,7 +84,7 @@ class NzButtonModel {
         borderWidth: borderWidth,
         borderRadius: borderRadius ?? 10,
         leftChildren: leftChildren,
-        rightChildren: leftChildren,
+        rightChildren: rightChildren,
         onTap: onTap,
       );
     } else {
@@ -104,7 +105,7 @@ class NzButtonModel {
         borderWidth: borderWidth,
         borderRadius: borderRadius ?? 10,
         leftChildren: leftChildren,
-        rightChildren: leftChildren,
+        rightChildren: rightChildren,
         onTap: onTap,
       );
     }
@@ -165,7 +166,67 @@ class NzButtonModel {
         borderWidth: borderWidth,
         borderRadius: borderRadius ?? 10,
         leftChildren: leftChildren,
-        rightChildren: leftChildren,
+        rightChildren: rightChildren,
+        onTap: onTap,
+      );
+    }
+  }
+  static NzButton iconButton(
+    BuildContext context,
+    String buttonName, {
+    bool hasText = false,
+    bool isActive = true,
+    double fontSize = 24,
+    FontWeight? fontWeight,
+    String buttonText = 'Button',
+    double? padding,
+    double borderWidth = 1,
+    double? borderRadius,
+    List<Widget>? leftChildren,
+    List<Widget>? rightChildren,
+    void Function()? onTap,
+  }) {
+    final theme = context.theme();
+    if (buttonName == 'main') {
+      return NzButton(
+        background: const Color.fromARGB(0, 255, 255, 255),
+        borderColour: const Color.fromARGB(0, 255, 255, 255),
+        fontColor: theme.ink.colour900!,
+        inactiveBackground: theme.ink.colour300!,
+        inactiveBorderColour: const Color.fromARGB(0, 255, 255, 255),
+        inactiveFontColor: theme.ink.colour700!,
+        hasBorder: false,
+        hasText: hasText,
+        isActive: isActive,
+        fontSize: fontSize,
+        fontWeight: fontWeight ?? FontWeight.w600,
+        buttonText: buttonText,
+        padding: padding ?? 4,
+        borderWidth: borderWidth,
+        borderRadius: borderRadius ?? 36,
+        leftChildren: leftChildren?? [SvgParser.parseSvg("x-icon", "you haven't selected any icon")],
+        rightChildren: rightChildren,
+        onTap: onTap,
+      );
+    } else {
+      return NzButton(
+        background: theme.positive.colour700!,
+        borderColour: theme.positive.colour700!,
+        fontColor: theme.background.colour100!,
+        inactiveBackground: theme.positive.colour500!,
+        inactiveBorderColour: theme.positive.colour500!,
+        inactiveFontColor: theme.background.colour100!,
+        hasBorder: false,
+        hasText: hasText,
+        isActive: isActive,
+        fontSize: fontSize,
+        fontWeight: FontWeight.w500,
+        buttonText: "Oops, you're selecting the wrong button",
+        padding: padding ?? 10,
+        borderWidth: borderWidth,
+        borderRadius: borderRadius ?? 10,
+        leftChildren: leftChildren,
+        rightChildren: rightChildren,
         onTap: onTap,
       );
     }
